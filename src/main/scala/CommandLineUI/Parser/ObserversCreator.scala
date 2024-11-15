ca0ab025430f8e08169653aed525f00a05dc0c57
Kryštof Jelínek <jelinkry@cvut.cz>
2024-11-15 19:42:11 +0100

    without reflection checkpoint

--- src/main/scala/CommandLineUI/Parser/ObserversCreator.scala
+++ src/main/scala/CommandLineUI/Parser/ObserversCreator.scala
@@ -1,33 +1,24 @@
 package CommandLineUI.Parser
 
 import CommandLineUI.CommandObservers.CommandObserver
+import CommandLineUI.CommandObservers.LoadCommandObservers.{LoadGifCommandObserver, LoadJpgCommandObserver, LoadPngCommandObserver, LoadRandomCommandObserver}
+import CommandLineUI.CommandObservers.SaveCommandObservers.{OutputConsoleCommandObserver, OutputFileCommandObserver}
+import CommandLineUI.CommandObservers.TransformCommandObservers.{BrightnessCommandObserver, InvertCommandObserver, NonLinearTableCommandObserver, ScaleCommandObserver, SetCustomTableCommandObserver, SetPredefinedTableCommandObserver}
 
 class ObserversCreator {
-  private val observerClasses = List(
-    "CommandLineUI.CommandObservers.LoadCommandObservers.LoadGifCommandObserver",
-    "CommandLineUI.CommandObservers.LoadCommandObservers.LoadJpgCommandObserver",
-    "CommandLineUI.CommandObservers.LoadCommandObservers.LoadPngCommandObserver",
-    "CommandLineUI.CommandObservers.LoadCommandObservers.LoadRandomCommandObserver",
-    "CommandLineUI.CommandObservers.SaveCommandObservers.OutputConsoleCommandObserver",
-    "CommandLineUI.CommandObservers.SaveCommandObservers.OutputFileCommandObserver",
-    "CommandLineUI.CommandObservers.TransformCommandObservers.BrightnessCommandObserver",
-    "CommandLineUI.CommandObservers.TransformCommandObservers.InvertCommandObserver",
-    "CommandLineUI.CommandObservers.TransformCommandObservers.NonLinearTableCommandObserver",
-    "CommandLineUI.CommandObservers.TransformCommandObservers.ScaleCommandObserver",
-    "CommandLineUI.CommandObservers.TransformCommandObservers.SetCustomTableCommandObserver",
-    "CommandLineUI.CommandObservers.TransformCommandObservers.SetPredefinedTableCommandObserver",
-  )
-
   def createObservers(): List[CommandObserver] = {
-    observerClasses.flatMap { observerName =>
-      try {
-        val observerClass = Class.forName(observerName)
-        val observer = observerClass.getDeclaredConstructor().newInstance()
-        Some(observer.asInstanceOf[CommandObserver])
-      } catch {
-        case e: Exception =>
-          throw RuntimeException(s"Could not load observer $observerName: ${e.getMessage}")
-      }
-    }
+    List[CommandObserver](
+      LoadGifCommandObserver(),
+      LoadJpgCommandObserver(),
+      LoadPngCommandObserver(),
+      LoadRandomCommandObserver(),
+      OutputConsoleCommandObserver(),
+      OutputFileCommandObserver(),
+      BrightnessCommandObserver(),
+      InvertCommandObserver(),
+      NonLinearTableCommandObserver(),
+      ScaleCommandObserver(),
+      SetCustomTableCommandObserver(),
+      SetPredefinedTableCommandObserver())
   }
 }
